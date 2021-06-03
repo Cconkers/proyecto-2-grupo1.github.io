67f8e0977dc1929955316cb63f3c48fba1e080b7
Your Name <you@example.com>
2021-06-03 23:03:55 +0200

    añadiendo interactividad

--- js/event_detailed_jason.js
+++ js/event_detailed_jason.js
@@ -1,3 +1,12 @@
 function desplegarHamburguesa(x) {
     x.classList.toggle("change");
   };
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  };
