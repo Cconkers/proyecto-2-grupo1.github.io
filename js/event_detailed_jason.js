091cfc01e1c0d0e673a79d6fdc8a2eb0e1360a45
Cconker <rubenpv011992@gmail.com>
2021-06-04 09:13:49 +0200

    adelante

---
+++ js/event_detailed_jason.js
@@ -0,0 +1,14 @@
+function desplegarHamburguesa(x) {
+    x.classList.toggle("change");
+  };
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  };
+
+  alert(jelo)
