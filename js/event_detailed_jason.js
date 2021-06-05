5cad354501e4170cf548cb91fa32813947035c21
unknown <derian12702@gmail.com>
2021-06-05 02:33:56 +0200

    Formulario de Contacto

---
+++ js/event_detailed_jason.js
@@ -0,0 +1,16 @@
+
+
+
+function desplegarHamburguesa(x) {
+    x.classList.toggle("change");
+  };
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  };
+
